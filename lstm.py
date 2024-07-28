e51e2a73d8d05c158f0418b576980b70f7006cd2
flinGAtech <fenglinGAtech@gmail.com>
2024-07-28 03:58:04 -0400

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 #pending test
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, prob_dropout =.2):
+    def __init__(self, input_size, hidden_size, num_layers, prob_dropout =.2):
         super(LSTM, self).__init__()
 
         self.hidden_size = hidden_size
@@ -13,7 +13,7 @@ class LSTM(nn.Module):
 
         self.dropout = nn.Dropout(prob_dropout)
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.fc = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
