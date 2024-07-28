d6b281a897f2b7762370fb8d62c17e4bce9e8f19
flinGAtech <fenglinGAtech@gmail.com>
2024-07-28 02:53:24 -0400

    Add files via upload

---
+++ lstm.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+#pending test
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTM, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).requires_grad_()
+
+        out = self.lstm(x, (h0, c0))
+        out = self.fc(out)
+        return out
